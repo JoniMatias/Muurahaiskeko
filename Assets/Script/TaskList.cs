0a55094803310a08d7326919192d8012363f0c96
Lookas123 <42605685+Lookas123@users.noreply.github.com>
2021-06-17 11:53:00 +0300

    a

---
+++ Assets/Script/TaskList.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskList
+{
+    private static TaskList _instance;
+
+    public static TaskList Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new TaskList();
+            }
+
+            return _instance;
+        }
+    }
+    public List<Task> tasklist;
+}
